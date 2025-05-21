c9ea9836a206d108938b7226e6c1e240148bf4d1
JihwanByun <jihwaja9091@naver.com>
2025-05-21 12:11:12 +0900

    new page

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,9 @@ import Recruit from "../pages/Recruit.vue";
 const routes = [
   { path: "/", name: "Main", component: Home },
   { path: "/result", name: "Result", component: Result },
-<<<<<<< HEAD
   { path: "/detail", name: "Detail", component: Detail },
   { path: '/jobs', name: 'Jobs', component: Recruit }]
   
-
-=======
-  { path: "/detail", name: "Detail", component: Detail, props: true },
->>>>>>> 0719182921a2daa0c09fea7ad0ed238cb61e4e71
-];
 
 const router = createRouter({
   history: createWebHistory(),
